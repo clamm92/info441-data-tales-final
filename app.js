e42a7662ef744d9a7c42059c1adb6261dfd04767
Connor Lam <clam92@uw.edu>
2025-10-24 17:18:31 -0700

    set up new project

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
